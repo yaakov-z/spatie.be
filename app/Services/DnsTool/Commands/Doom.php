c416ae5bd45c3adb69b64d750876061cad99c325
freek <freek@spatie.be>
2020-12-04 13:34:53 +0100

    wip

--- app/Services/DnsTool/Commands/Doom.php
+++ app/Services/DnsTool/Commands/Doom.php
@@ -14,6 +14,6 @@ class Doom implements Command
 
     public function perform(string $command): Response
     {
-        return redirect('https://js-dos.com/games/doom.exe.html');
+        return redirect('https://dos.zone/en/play/https%3A%2F%2Fdoszone-uploads.s3.dualstack.eu-central-1.amazonaws.com%2Foriginal%2F2X%2Fe%2Fede529c8e71c94363f9f3c8fd84519608bd632ea.jsdos?turbo=0');
     }
 }
